a4aa0b531b29d2f04214a29acc6f536d23dfe7cb
Cahlan Sharp <cahlan@gmail.com>
2012-02-02 21:14:18 -0700

    updated setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-setup(name = "python-linkedin",
-      version = "1.8.2",
-      description = "Python Interface for the LinkedIn API",
-      author = "Ozgur Vatansever",
+setup(name = "linkedin-python-fork",
+      version = "1.8.2.0",
+      description = "Python Interface for the LinkedIn API (includes network updates)",
+      author = "Cahlan Sharp",
       license = open("LICENSE").read(),
       long_description = open("README").read(),
-      author_email = "ozgurvt@gmail.com",
-      url = "http://code.google.com/p/python-linkedin/",
+      author_email = "cahlan@gmail.com",
+      url = "https://github.com/cahlan/linkedin-python-fork/",
       packages = ["linkedin"],
       platforms = ["linux"])
